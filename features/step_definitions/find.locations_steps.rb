c992e5bef611145cf6186c5d72704113594b5efc
aqspwc <andrew.q.smith@us.pwc.com>
2014-11-13 13:45:16 -0500

    failuremode

--- features/step_definitions/find.locations_steps.rb
+++ features/step_definitions/find.locations_steps.rb
@@ -1,5 +1,5 @@
 Given(/^I am on (.+)$/) do |url|
-  visit ('http://www.va.gov/bluebutton/') #open firefox and go to va.gov
+  visit ('http://www.va.gov/bluebutton12343/') #open firefox and go to va.gov
 end
 
 When(/^I enter my zip code$/) do
